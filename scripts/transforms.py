52c6fe5913b68c7286ec1c77e410b5dca21256d7
anibalsande <anibal.sande@udc.es>
2026-04-19 19:52:31 +0200

    Update transforms.py

--- scripts/transforms.py
+++ scripts/transforms.py
@@ -11,6 +11,7 @@ base_transform = t.Compose([
 aug_transform = t.Compose([
     t.Resize((224, 224)),
     t.RandomHorizontalFlip(p=0.5),
+    t.RandomRotation(degrees=5),
     t.ColorJitter(brightness=0.2, contrast=0.2),
     t.ToTensor(),
     normalize
